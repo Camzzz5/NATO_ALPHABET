e828fa3d72c6854a1a8e9d90a20d444dceffbd4e
Camilo Santander <camilosantander@usantotomas.edu.co>
2023-01-29 17:22:02 -0500

    final tweaks

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import pandas as pd
 
 data = pd.read_csv("nato_phonetic_alphabet.csv")
-data_dict = {key: value for (key, value) in zip(data.letter, data.code)}
+#data_dict = {key: value for (key, value) in zip(data.letter, data.code)}
+data_dict = {row.letter: row.code for (index, row) in data.iterrows()}
 #print(data_dict)
 word = input("ENTER A WORD: \n").upper()
 data_list = [data_dict[letter] for letter in word]
