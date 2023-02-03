344e57d7339f8674b8101537670931da15eab284
Camilo Santander <camilosantander@usantotomas.edu.co>
2023-02-02 22:46:11 -0500

    added a try/cath statement

--- main.py
+++ main.py
@@ -4,7 +4,14 @@ data = pd.read_csv("nato_phonetic_alphabet.csv")
 #data_dict = {key: value for (key, value) in zip(data.letter, data.code)}
 data_dict = {row.letter: row.code for (index, row) in data.iterrows()}
 #print(data_dict)
-word = input("ENTER A WORD: \n").upper()
-data_list = [data_dict[letter] for letter in word]
-print(data_list)
+a = True
+while a:
+    try:
+        word = input("ENTER A WORD: \n").upper()
+        data_list = [data_dict[letter] for letter in word]
+        a = False
+    except KeyError:
+        print("Sorry, only letters in the alphabet are supported")
+    else:
+        print(data_list)
 
